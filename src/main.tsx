ac5b0d0f961d4d567b274c0a175a504b5160d22b
Nguyễn Sơn <nhson.codes@gmail.com>
2025-02-15 09:08:15 +0700

    🔧 Config react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
+// Import from react
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Import third party
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Styles
 import './index.css'
+
+// Import Custom
 import App from './App.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient: QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {import.meta.env.DEV && <ReactQueryDevtools position="bottom" initialIsOpen={false} />}
+    </QueryClientProvider>
   </StrictMode>,
 )
