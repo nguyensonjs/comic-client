851f85a743ca413106dfec1ffdebec58b20921a3
Nguyễn Sơn <nhson.codes@gmail.com>
2025-02-15 08:46:24 +0700

    🎨 Format code

--- alias.config.ts
+++ alias.config.ts
@@ -1,8 +1,8 @@
-import { resolve } from "path";
+import { resolve } from 'path'
 
 const alias: Record<string, string> = {
-  "@components": resolve(__dirname, "src/components"),
-  "@utils": resolve(__dirname, "src/utils"),
-};
+  '@components': resolve(__dirname, 'src/components'),
+  '@utils': resolve(__dirname, 'src/utils')
+}
 
-export default alias;
+export default alias
