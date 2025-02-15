1c19d75cfdb1f3b6d81fd75703e13a5f425958bc
Nguyễn Sơn <nhson.codes@gmail.com>
2025-02-15 09:15:16 +0700

    💄 Update UI React-Query-Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 // Import third party
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Styles
@@ -10,7 +11,6 @@ import './index.css'
 
 // Import Custom
 import App from './App.tsx'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient: QueryClient = new QueryClient({
   defaultOptions: {
@@ -25,7 +25,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      {import.meta.env.DEV && <ReactQueryDevtools position="bottom" initialIsOpen={false} />}
+      {import.meta.env.DEV && (
+        <ReactQueryDevtools
+          buttonPosition="bottom-right"
+          position="bottom"
+          initialIsOpen={false}
+        />
+      )}
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 )
