ea755648f6cea40eac702a8f5a63efc7cadcc68c
Nguyễn Sơn <nhson.codes@gmail.com>
2025-02-15 08:46:03 +0700

    🔧 Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,45 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import eslintPluginImport from "eslint-plugin-import";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import eslintPluginImport from 'eslint-plugin-import'
+import tanstackQuery from '@tanstack/eslint-plugin-query'
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{js,ts,tsx}"],
+    ignores: ['dist', 'node_modules']
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
+      parser: '@typescript-eslint/parser',
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     settings: {
-      "import/resolver": {
+      'import/resolver': {
         typescript: {
-          project: "./tsconfig.json",
-        },
-      },
+          project: './tsconfig.json'
+        }
+      }
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       import: eslintPluginImport,
+      '@tanstack/query': tanstackQuery
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
       ],
-      "import/no-unresolved": "error",
-    },
+      'import/no-unresolved': 'error',
+      '@tanstack/query/exhaustive-deps': 'warn'
+    }
   }
-);
+]
