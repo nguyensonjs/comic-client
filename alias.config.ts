02488147cd411659289242e4ad80c793de1f6c92
Nguyễn Sơn <nhson.codes@gmail.com>
2025-02-15 08:49:06 +0700

    🔧 Update alias

--- alias.config.ts
+++ alias.config.ts
@@ -2,7 +2,8 @@ import { resolve } from 'path'
 
 const alias: Record<string, string> = {
   '@components': resolve(__dirname, 'src/components'),
-  '@utils': resolve(__dirname, 'src/utils')
+  '@utils': resolve(__dirname, 'src/utils'),
+  '@assets': resolve(__dirname, 'src/assets')
 }
 
 export default alias
